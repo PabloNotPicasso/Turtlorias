8867a205ba1f728cb78113b05024f05b9f8854d9
PabloNotPicasso <bugaev99@gmail.com>
2020-04-29 19:10:47 +0300

    Some tricks to use new-expression and new-operator

---
+++ NewExpression/NewExpression/main.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Function to call on bad_alloc exception
+void noMemory()
+{
+    cout << "noMemory Called\n";
+}
+
+class A
+{
+public:
+    A():a(1){}
+    int a;
+};
+
+int main()
+{
+    // allocation and constr
+    A* p1 = new A;
+    cout << p1->a;
+
+    // allocation and constr noexcept
+    A* p2 = new(nothrow) A;
+    cout << p2->a;
+
+    // allocation, NO constr
+    A* p3 = (A*)::operator new(sizeof(A));
+    cout << p3->a;
+
+    // NO allocation, constr
+    new(p3) A;
+    cout << p3->a;
+
+
+
+    // function to call if new cannot allocate the memory
+    std::set_new_handler( noMemory );
+    // nothrow param - making new noexcept
+    // On bad_alloc  - returns 0
+    int *a = new(std::nothrow) int[ 0x7FFFFFFF ]; // try to allocate 8GB memory
+    if( !a )
+    {
+        std::cout << "Cannot allocate 2Gb\n"; // Its impossible to allocate the memory
+    }
+    if( a )
+    {
+        std:: cout << "Success allocate 2Gb\n";
+        delete[] a;
+    }
+
+    return 0;
+}
