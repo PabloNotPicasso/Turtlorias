a1675db7ccadd2189986a1e72850d86b38a6e900
PabloNotPicasso <bugaev99@gmail.com>
2023-06-27 23:39:21 +0300

    Fix docker image build. Codeformat fix

--- Structures&Algorithms/src/Geometry/Point.h
+++ Structures&Algorithms/src/Geometry/Point.h
@@ -1,49 +1,55 @@
 #pragma once
 
-class Point
-{
+class Point {
 public:
-  double x, y;
+    double x, y;
 
-  Point()
-    : x(0), y(0){}
+    Point()
+        : x(0)
+        , y(0)
+    {
+    }
 
-  Point(const double _x, const double _y)
-    : x(_x), y(_y){}
+    Point(const double _x, const double _y)
+        : x(_x)
+        , y(_y)
+    {
+    }
 
-  Point operator-() const {
-    return {-x, -y};
-  }
+    Point operator-() const
+    {
+        return {-x, -y};
+    }
 
-  double distance() const
-  {
-    return sqrt( x * x + y * y );
-  }
+    double distance() const
+    {
+        return sqrt(x * x + y * y);
+    }
 
-  static double distance(const Point a, const Point b)
-  {
-    double x = b.x - a.x;
-    double y = b.y - a.y;
-    return sqrt( x * x + y * y );
-  }
+    static double distance(const Point a, const Point b)
+    {
+        double x = b.x - a.x;
+        double y = b.y - a.y;
+        return sqrt(x * x + y * y);
+    }
 
-  static Point createRandomPoint()
-  {
-    double x = rand() % 5000;
-    double y = rand() % 5000;
+    static Point createRandomPoint()
+    {
+        double x = rand() % 5000;
+        double y = rand() % 5000;
 
-    return {x/10, y/10};
-  }
+        return {x / 10, y / 10};
+    }
 };
 
 std::istream& operator>>(std::istream& is, Point& p)
 {
-  is >> p.x >> p.y;
-  return is;
+    is >> p.x >> p.y;
+    return is;
 }
 
 std::ostream& operator<<(std::ostream& os, const Point p)
 {
-  os << "(" << p.x << ", " << p.y << ")";
-  return os;
+    os << "(" << p.x << ", " << p.y << ")";
+    return os;
 }
--- Structures&Algorithms/src/Geometry/Ray.h
+++ Structures&Algorithms/src/Geometry/Ray.h
@@ -2,34 +2,32 @@
 
 #include "BaseLine.h"
 
-class Ray
-{
+class Ray {
 public:
-  Point start;
-  Point direction;
+    Point start;
+    Point direction;
 
-  bool belongs(const Point p) const
-  {
-    Vector v1(start, direction);
-    Vector v2(start, p);
-
-    return (0 == Vector::vectorProduct(v1, v2)) &&
-            // scalar between (start, p) and (start, direction) should be >=0 cause angle is ~0*
-           (Vector::scalar(v1, v2) >= 0);
-  }
-
-  double distanceTo(const Point p) const
-  {
-    Vector directionVec(start, direction);
-    Vector toPoint(start, p);
-
-    bool isProjected = Vector::scalar(directionVec, toPoint) >= 0;
-
-    if ( !isProjected )
+    bool belongs(const Point p) const
     {
-      return Point::distance(p, start);
+        Vector v1(start, direction);
+        Vector v2(start, p);
+
+        return (0 == Vector::vectorProduct(v1, v2)) &&
+            // scalar between (start, p) and (start, direction) should be >=0 cause angle is ~0*
+            (Vector::scalar(v1, v2) >= 0);
     }
-    // hipotenusa * sinus
-    return toPoint.len() * fabs( Vector::sinBetween(directionVec, toPoint) );
-  }
+
+    double distanceTo(const Point p) const
+    {
+        Vector directionVec(start, direction);
+        Vector toPoint(start, p);
+
+        bool isProjected = Vector::scalar(directionVec, toPoint) >= 0;
+
+        if (!isProjected) {
+            return Point::distance(p, start);
+        }
+        // hipotenusa * sinus
+        return toPoint.len() * fabs(Vector::sinBetween(directionVec, toPoint));
+    }
 };
