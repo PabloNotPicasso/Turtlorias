053ac3a96df464af728d168b6e9f49a73d26faaa
PabloNotPicasso <bugaev99@gmail.com>
2023-10-20 20:41:16 +0300

    Add draft Allocator

---
+++ StructuresAndAlgorithms/src/Structures/STL/Allocator/Allocator.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <utility>
+
+namespace Structures::STL {
+
+template<typename T>
+class Allocator {
+public:
+    using value_type = T;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using reference = T&;
+    using const_reference = const T&;
+    using size_type = size_t;
+
+public:
+    [[nodiscard]] T* allocate(std::size_t n)
+    {
+        return reinterpret_cast<T*>(::operator new(n * sizeof(T)));
+    }
+
+    void deallocate(T* ptr, std::size_t n)
+    {
+        ::operator delete(ptr, n);
+    }
+
+    template<typename... Args>
+    void construct(T* p, Args... args)
+    {
+        new (p) T(std::forward<Args>(args)...);
+    }
+
+    void destroy(T* p)
+    {
+        p->~T();
+    }
+};
+
+} // namespace Structures::STL
---
+++ StructuresAndAlgorithms/src/Structures/STL/Allocator/test/AllocatorTest.cpp
@@ -0,0 +1,28 @@
+#include "Structures/STL/Allocator/Allocator.h"
+#include "Structures/STL/Vector/Vector.h"
+
+#include <gtest/gtest.h>
+
+using namespace Structures::STL;
+
+class Dummy {
+public:
+    static constexpr std::string_view initialDataString = "InitialDataString";
+    std::string data;
+
+    Dummy()
+        : Dummy(initialDataString)
+    {
+    }
+
+    Dummy(const std::string_view& dataToInitialize)
+        : data(dataToInitialize)
+    {
+    }
+};
+
+TEST(Allocator, vectorTest)
+{
+    const size_t size = 10;
+    Vector<Dummy, Allocator<Dummy>> vector(size);
+}
